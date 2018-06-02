0aad482414381cddb279ee7bc62711b6985d4b31
TARUN GOYAL <goyaltarun584@gmail.com>
2018-06-03 04:40:45 +0530

    commit 2

--- app/src/main/java/com/example/tarungoyal/chatein/UsersActivity.java
+++ app/src/main/java/com/example/tarungoyal/chatein/UsersActivity.java
@@ -2,12 +2,24 @@ package com.example.tarungoyal.chatein;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class UsersActivity extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar mToolbar;
     private RecyclerView mUsersList;
+    private DatabaseReference mUsersDatabase;
 
 
     @Override
@@ -19,8 +31,64 @@ public class UsersActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("All Users");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mUsersList = (RecyclerView)findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startListening();
+
+    }
+    public void startListening(){
+        Query query = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("Users")
+                .limitToLast(50);
+
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(query, Users.class)
+                        .build();
+        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<Users, UserViewHolder>(options) {
+            @Override
+            public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                // Create a new instance of the ViewHolder, in this case we are using a custom
+                // layout called R.layout.message for each item
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.users_single_layout, parent, false);
+
+                return new UserViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(UserViewHolder holder, int position, Users model) {
+                // Bind the Chat object to the ChatHolder
+                holder.setName(model.name);
+                // ...
+            }
+
+        };
+        mUsersList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        public UserViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setName(String name){
+
+            TextView userNameView = mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+            }
     }
 }
